5b07f92a584441f2d7699122eccabf80c8c098be
israelcgomar <israel5837@hotmail.com>
2019-01-06 22:49:22 -0600

    ^ fix reaload articles

--- src/app/Allarticles/allArticles.js
+++ src/app/Allarticles/allArticles.js
@@ -1,76 +1,87 @@
-import React, { Component } from "react";
-import { InputSearch } from "../Shared/Inputs/Inputs";
-import "./allArticles.sass";
+import React, { Component } from 'react';
+import { InputSearch } from '../Shared/Inputs/Inputs';
+import './allArticles.sass';
 // import LoaderComponent from "../Shared/Loader";
 
-
-const API = "http://localhost:8010/api/v1/";
+const API = 'http://localhost:8010/api/v1/';
 
 export default class Articles extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      search: "",
-      timeLoader: ''
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			articles: [],
+			search: '',
+			timeLoader: ''
+		};
+	}
+
+	componentWillMount() {
+		localStorage.getItem('articles') && this.setState(JSON.parse(localStorage.getItem('articles')));
+	}
+
+	componentDidMount() {
+		const date = localStorage.getItem('contactsDate');
+		const contactsDate = date && new Date(parseInt(date));
+		const now = new Date();
+
+		const dataAge = Math.round((now - contactsDate) / (1000 * 60)); // in minutes
+		const tooOld = dataAge >= 1;
+
+		if (tooOld) {
+			// this.fetchData();
+		} else {
+			console.log(`Using data from localStorage that are ${dataAge} minutes old.`);
+		}
+	}
+
   
-  
-  onchange = e => {
-    this.setState({ search: e.target.value });
-    setTimeout(() => {
-      const url = `${API}articles-liverpool/${this.state.search}`;
-      fetch(url)
-        .catch(err => err)
-        .then(res => res.json())
-        .then(dataArticles => this.onSetResult(dataArticles));
-    }, 3000);
-  };
+	onchange = (e) => {
+		this.setState({ search: e.target.value });
+		setTimeout(() => {
+			const url = `${API}articles-liverpool/${this.state.search}`;
+			fetch(url)
+				.catch((err) => err)
+				.then((res) => res.json())
+				.then((articles) => this.setState(articles));
+		}, 3000);
+	};
 
-  onSetResult = (result, key) => {
-      localStorage.setItem('articles', JSON.stringify(result));
-      var articles = JSON.parse(localStorage.getItem('articles'));  
-      this.setState(articles);
-  }
+	// onSetResult = (articles, key) => {
+	// 	this.setState(articles);
+	// };
 
+	componentWillUpdate(nextProps, nextState) {
+		localStorage.setItem('articles', JSON.stringify(nextState.articles));
+		localStorage.setItem('articlesDate', Date.now());
+	}
 
-  renderArticles = article => {
-    return (
-      <div className="Articles custom__card">
-        <h3 className="lead"> {article.name}</h3>
-        <p className="lead__gray">{article.price}</p>
-        <div className="img__content">
-          <img className="article__img" src={article.img} alt="6_dias" />
-        </div>
-        <p className="lead__gray">Finaliza: 03 de julio 2015</p>
-        <button className="btn__table btn__detail">Detalle</button>
-      </div>
-    );
-  };
+	renderArticles = (article) => {
+		return (
+			<div className="Articles custom__card">
+				<h3 className="lead"> {article.name}</h3>
+				<p className="lead__gray">{article.price}</p>
+				<div className="img__content">
+					<img className="article__img" src={article.img} alt="6_dias" />
+				</div>
+				<p className="lead__gray">Finaliza: 03 de julio 2015</p>
+				<button className="btn__table btn__detail">Detalle</button>
+			</div>
+		);
+	};
 
-  render() {
-    const { search } = this.state;
-    console.log(this.state.articles);
-    
-    // if (this.state.articles.length > 0) {
-      return <div>
-          <InputSearch 
-          name='search'
-          label="Buscar Articulos" 
-          value={search}
-          onChange={this.onchange}
-          />
-          <br />
-          <div className="container__articles">
-          {this.state.articles && this.state.articles.map((article, i) => {
-              return <div key={i}>{this.renderArticles(article)}</div>;
-            })}
-          {/* <LoaderComponent /> */}
-          </div>
-        </div>;
-    // } else {
-    //   return <LoaderComponent />
-    // }
-  }
+	render() {
+		const { search, articles } = this.state;
+		return (
+			<div>
+				<InputSearch name="search" label="Buscar Articulos" value={search} onChange={this.onchange} />
+				<br />
+				<div className="container__articles">
+					{articles &&
+						articles.map((article, i) => {
+							return <div key={i}>{this.renderArticles(article)}</div>;
+						})}
+				</div>
+			</div>
+		);
+	}
 }
